ed9447ccf6b7ed1cc11d340d64e211e765f43a1c
vamshi <vamshik889@gmail.com>
2024-12-26 10:23:45 +0530

    js practice

---
+++ PROBLEM_IMP/Uniqueelements.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5,6,1,2,3,4,5,6,7,8,9]
+
+function removeDup(arr){
+  let newArr = [];
+
+  for(let i=0;i<arr.length;i++){
+
+    if(newArr.indexOf(arr[i]) == -1){
+      newArr.push(arr[i])
+    }
+  }
+
+  return newArr;
+}
+
+let dup = removeDup(arr);
+console.log(dup)
--- Practice/14jul.js
+++ Practice/14jul.js
@@ -1,12 +1,22 @@
-let arr2 = [1,2,3,4,5];
+// let arr2 = [1,2,3,4,5];
 
-arr2.forEach(function(element){
-    if(element%2 ===0){
-        console.log(element);
-    }
-}) ;
+// arr2.forEach(function(element){
+//     if(element%2 ===0){
+//         console.log(element);
+//     }
+// }) ;
 
-let mapPract = arr2.map(function(elemen){
-return elemen*2;
+// let mapPract = arr2.map(function(elemen){
+// return elemen*2;
+// });
+// console.log(mapPract);
+
+
+let arr3 = [1,2,3,4,5,5,5]
+
+let rrr = arr3.filter((e,i)=>{
+    
+        return e%2==0;
+    
 });
-console.log(mapPract);
+console.log(rrr)
